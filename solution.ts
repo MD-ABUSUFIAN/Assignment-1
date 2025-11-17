a8bc2c3fc6dd413f8c3cacbc304aa2c463a356c5
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2025-11-17 17:42:41 +0600

    problem 1-8 solved

--- solution.ts
+++ solution.ts
@@ -1,4 +1,4 @@
-// problem:1
+
 type Union = string | number | boolean;
 const formatValue = (value: Union): Union => {
   if (typeof value === 'string') {
@@ -10,18 +10,18 @@ const formatValue = (value: Union): Union => {
   }
 };
 
-// problem:2
+
 
 const getLength = (value: string | unknown[]): number => {
   if (typeof value === 'string') {
-    const splitString = value.split('').length;
-    return splitString;
+    return value.length;
   } else {
     return value.length;
   }
 };
 
-// problem:3
+
+
 class Person {
   name: string;
   age: number;
@@ -34,15 +34,16 @@ class Person {
   }
 }
 
-// problem:4
-const filterByRating = (items: Items[]): Items[] => {
-  const filterItem = items.filter((item) => item.rating >= 4);
-  return filterItem;
-};
+
+
 
 type Items = {
   title: string;
   rating: number;
+};
+const filterByRating = (items: Items[]): Items[] => {
+  const filterItem = items.filter((item) => item.rating >= 4);
+  return filterItem;
 };
 const books: Items[] = [
   { title: 'Book A', rating: 4.5 },
@@ -50,7 +51,8 @@ const books: Items[] = [
   { title: 'Book C', rating: 5.0 },
 ];
 
-// problem:5
+
+
 
 type User = {
   id: number;
@@ -70,7 +72,7 @@ const users: User[] = [
   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
 ];
 
-// Problem:6
+
 
 interface Book {
   title: string;
@@ -90,3 +92,89 @@ const myBook: Book = {
   publishedYear: 1925,
   isAvailable: true,
 };
+
+
+
+
+
+const getUniqueValues = (array1: number[], array2: number[]) => {
+  const newArray: number[] = [];
+  let noOfItem = 0;
+  for (let i = 0; i < array1.length; i++) {
+    let isExistItem = false;
+    for (let j = 0; j < newArray.length; j++) {
+      if (newArray[j] === array1[i]) {
+        isExistItem = true;
+        break;
+      }
+    }
+    if (!isExistItem) {
+      newArray[noOfItem] = array1[i];
+      noOfItem++;
+    }
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    let isExistItem = false;
+    for (let j = 0; j < newArray.length; j++) {
+      if (array2[i] === newArray[j]) {
+        isExistItem = true;
+        break;
+      }
+    }
+    if (!isExistItem) {
+      newArray[noOfItem] = array2[i];
+      noOfItem++;
+    }
+  }
+
+  for (let i = 0; i < noOfItem; i++) {
+    for (let j = 0; j < noOfItem - i - 1; j++) {
+      if (newArray[j] > newArray[j + 1]) {
+        const temp = newArray[j];
+        newArray[j] = newArray[j + 1];
+        newArray[j + 1] = temp;
+      }
+    }
+  }
+
+  return newArray;
+};
+
+const array1: number[] = [1, 5, 8, 8, 3];
+const array2: number[] = [3, 4, 5, 6, 7, 9, 4, 2];
+
+
+
+
+
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+const calculateTotalPrice = (products: Product[]): number => {
+  if (products.length === 0) {
+    return 0;
+  }
+
+  const totalPrice = products.reduce((subTotal, product) => {
+    const { price, quantity, discount } = product;
+
+    if (discount) {
+      const discountPrice = price - (price * discount) / 100;
+      subTotal += discountPrice * quantity;
+    } else {
+      subTotal += price * quantity;
+    }
+    return subTotal;
+  }, 0);
+  return totalPrice;
+};
+
+const products: Product[] = [
+  { name: 'Pen', price: 10, quantity: 2 },
+  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
